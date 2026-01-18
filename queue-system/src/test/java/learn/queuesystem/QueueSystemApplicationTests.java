34616381aeb4a914dc29229ae8cfc80a4f764aad
eunseong <dmstjd0214@naver.com>
2026-01-18 14:08:00 +0900

    add : "기본 프로젝트 생성"

---
+++ queue-system/src/test/java/learn/queuesystem/QueueSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.queuesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
